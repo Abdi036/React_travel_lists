3209d5f058e9e6dba38159b85996c32ab89d0412
Abdi kumela <abdikumela@036gmail.com>
2024-01-24 17:31:44 -0800

    added some features to the main content

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: "passport", quantiyty: 2, packed: true },
+  { id: 2, description: "shirt", quantiyty: 4, packed: false },
+  { id: 3, description: "charger", quantiyty: 1, packed: false },
+  { id: 4, description: "socks", quantiyty: 5, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +31,7 @@ function Logo() {
 function Form() {
   return (
     <div className="form_wrapper">
-      <h3>FORM</h3>
+      <p>What do you need for your trip?</p>
     </div>
   );
 }
@@ -32,11 +39,25 @@ function Form() {
 // PackingList Component
 function PackingList() {
   return (
-    <main className="list_wrapper">
-      <h3>LISTS</h3>
-      <h3>LISTS</h3>
-      <h3>LISTS</h3>
+    <main className="list_container">
+      <div className="list_wrapper">
+        {initialItems.map((item) => (
+          <List item={item} />
+        ))}
+      </div>
     </main>
+  );
+}
+
+// component for List
+function List({ item }) {
+  return (
+    <span className="lists">
+      <i style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description}
+      </i>
+      <span>&times;</span>
+    </span>
   );
 }
 
